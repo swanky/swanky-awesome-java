99151f6fcfbc4077a1a8d6f247ab5a2850ae5380
Swanky <swanky.hsiao@gmail.com>
2016-08-05 09:11:13 +0800

    init

---
+++ src/main/java/io/github/swanky/SwankyAwesomeJavaApplication.java
@@ -0,0 +1,12 @@
+package io.github.swanky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwankyAwesomeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwankyAwesomeJavaApplication.class, args);
+	}
+}
